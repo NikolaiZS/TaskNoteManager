2d4a7184a7eba85a191f1e38e80317f870760b8c
BLACKUM <Zipmaster2852@gmail.com>
2024-11-16 18:53:47 +0300

    Added pages and logic for navigation 🛠
    Added pages for projects and settings, fixed logic for navigation view and fixed some design issues

--- TNM/Menu/MainMenu.xaml.cs
+++ TNM/Menu/MainMenu.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Wpf.Ui.Controls;
-using TNM.Samples;
+using TNM.Pages;
 using System.Collections.ObjectModel;
 
 namespace TNM.Menu
---
+++ TNM/Pages/ProjectView.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace TNM.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ProjectView.xaml
+    /// </summary>
+    public partial class ProjectView : Page
+    {
+        public ProjectView()
+        {
+            InitializeComponent();
+
+            // Пример списка проектов
+            var projects = new List<Project>
+            {
+                new Project { Title = "Проект 1", Description = "Описание проекта 1" },
+                new Project { Title = "Проект 2", Description = "Описание проекта 2" },
+                new Project { Title = "Проект 3", Description = "Описание проекта 3" },
+                new Project { Title = "Проект 4", Description = "Описание проекта 4" },
+                new Project { Title = "Проект 5", Description = "Описание проекта 5" },
+            };
+
+            ProjectList.ItemsSource = projects;
+        }
+        private void CreateNewProject_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
+    }
+
+    // Класс для хранения данных о проекте
+    public class Project
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
