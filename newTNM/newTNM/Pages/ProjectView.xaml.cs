59b888a5778d26ea188f6d57560b7e006b8302f4
NikolaiZS <goodnightbozos@gmail.com>
2024-11-24 19:49:16 +0300

    Update ProjectView.xaml.cs

--- newTNM/newTNM/Pages/ProjectView.xaml.cs
+++ newTNM/newTNM/Pages/ProjectView.xaml.cs
@@ -18,57 +18,57 @@ using newTNM.Menu;
 namespace newTNM.Pages
 {
     /// <summary>
-    /// Логика взаимодействия для ProjectView.xaml
+    // Логика взаимодействия для ProjectView.xaml
     /// </summary>
     public partial class ProjectView : Page
     {
         public ProjectView()
         {
             InitializeComponent();
-            //LoadProject();
+            LoadProject();
 
         }
         private void CreateNewProject_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Test");
         }
-        //private void LoadProject()
-        //{
-        //    using (var context = new TaskNoteManagementDBEntities())
-        //    {
-        //        var projects = context.Projects
-        //            .Select(p => new { p.ProjectName, p.Description })
-        //            .ToList();
+        private void LoadProject()
+        {
+            using (var context = new TaskNoteManagementDBEntities())
+            {
+                var projects = context.Projects
+                    .Select(p => new { p.ProjectName, p.Description })
+                    .ToList();
 
-        //        // Установите результат как ItemsSource для элемента UI
-        //        ProjectList.ItemsSource = projects;
-        //    }
+                // Установите результат как ItemsSource для элемента UI
+                ProjectList.ItemsSource = projects;
+            }
 
-        //}
+        }
         private void EditProject_Click(object sender, RoutedEventArgs e)
         {
             var button = sender as Button;
-            //var project = button?.DataContext as Projects;
+            var project = button?.DataContext as Projects;
 
-            //if (project != null)
-            //{
-            //    var mainWindow = Application.Current.MainWindow as MainMenu;
-            //    if (mainWindow != null)
-            //    {
-            //        // Добавим отладочный вывод
-            //        MessageBox.Show("Передача проекта в EditProject");
+            if (project != null)
+            {
+                var mainWindow = Application.Current.MainWindow as MainMenu;
+                if (mainWindow != null)
+                {
+                    // Добавим отладочный вывод
+                    MessageBox.Show("Передача проекта в EditProject");
 
-            //        // Передаем проект в статическое свойство SelectedProject
-            //        EditProject.SelectedProject = project;
+                    // Передаем проект в статическое свойство SelectedProject
+                    EditProject.SelectedProject = project;
 
-            //        // Навигация на страницу EditProject
-            //        mainWindow.NavigationMenu.Navigate(typeof(EditProject));
-            //    }
-            //}
-            //else
-            //{
-            //    MessageBox.Show("Проект не найден!");
-            //}
+                    // Навигация на страницу EditProject
+                    mainWindow.NavigationMenu.Navigate(typeof(EditProject));
+                }
+            }
+            else
+            {
+                MessageBox.Show("Проект не найден!");
+            }
         }
 
 
